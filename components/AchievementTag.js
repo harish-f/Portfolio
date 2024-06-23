45aa4df7a16495dbbe3a0e9349bc87ca11d314c4
Harish Ram <harish@Harishs-MacBook-Air.local>
2024-06-23 23:09:08 +0800

    added padding for tag

--- components/AchievementTag.js
+++ components/AchievementTag.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 export default function AchievementTag({ tag: { award, colour, shadow } }) {
     return (
         <p
-            className={"font-bold text-white h-min px-2 rounded-full absolute bottom-3 right-3"}
+            className={"font-bold text-white h-min px-3 py-1 rounded-full absolute bottom-3 right-3"}
             style={{ backgroundColor: colour }}
         >
             {award}
