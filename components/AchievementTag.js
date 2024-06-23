5a068d229c5c017d7e446b4cd991f1300ca4144a
Harish Ram <harish@Harishs-MacBook-Air.local>
2024-06-23 22:44:38 +0800

    actually fixed bgcolor bug

--- components/AchievementTag.js
+++ components/AchievementTag.js
@@ -1,16 +1,12 @@
 import { useEffect } from "react"
 import { useState } from "react"
 
-export default function AchievementTag({ tag: {award, colour, shadow} }) {
-    const [tagClass, setTagClass] = useState("")
-
-    useEffect( () => {
-        setTagClass(`bg-[${colour}] font-bold text-white h-min px-2 rounded-full absolute bottom-3 right-3`)
-    }, [])
-
-    console.log(tagClass)
+export default function AchievementTag({ tag: { award, colour, shadow } }) {
     return (
-        <p className={tagClass}>
+        <p
+            className={"font-bold text-white h-min px-2 rounded-full absolute bottom-3 right-3"}
+            style={{ backgroundColor: colour }}
+        >
             {award}
         </p>
     )
