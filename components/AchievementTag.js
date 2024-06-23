9531e7d30b4e99981c2917a29be8bf4a15a24ded
Harish Ram <harish@Harishs-MacBook-Air.local>
2024-06-23 22:27:22 +0800

    fixed bug where achievement tag doesnt render with colour

--- components/AchievementTag.js
+++ components/AchievementTag.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 export default function AchievementTag({ tag: {award, colour, shadow} }) {
+    const [tagClass, setTagClass] = useState("")
+
+    useEffect( () => {
+        setTagClass(`bg-[${colour}] font-bold text-white h-min px-2 rounded-full absolute bottom-3 right-3`)
+    }, [])
+
+    console.log(tagClass)
     return (
-        <p className={`bg-[${colour}] font-bold text-white h-min px-2 rounded-full absolute bottom-3 right-3`}>
+        <p className={tagClass}>
             {award}
         </p>
     )
