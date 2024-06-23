73e43956b28423a90c823114096501fd487815aa
Harish Ram <harish@Harishs-MacBook-Air.local>
2024-06-24 00:52:28 +0800

    changed margin

--- components/ProjectLinkIcon.js
+++ components/ProjectLinkIcon.js
@@ -1,7 +1,7 @@
 export default function ProjectLinkIcon({ link, image }) {
         return (
             <a target="_blank" href={link}>
-                <img className="rounded-full object-cover aspect-square w-14 mt-5" src={image} />
+                <img className="rounded-full object-cover aspect-square w-14 mt-3" src={image} />
             </a>
         )
 }
