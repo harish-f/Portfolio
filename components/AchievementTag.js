0569c8fc449d54314a3d0e95847b43116db0a69a
Harish Ram <harish@Harishs-MacBook-Air.local>
2024-06-24 00:51:56 +0800

    added conditional for appearance

--- components/AchievementTag.js
+++ components/AchievementTag.js
@@ -2,12 +2,16 @@ import { useEffect } from "react"
 import { useState } from "react"
 
 export default function AchievementTag({ tag: { award, colour, shadow } }) {
-    return (
-        <p
-            className={"font-bold text-white h-min px-3 py-1 rounded-full absolute bottom-3 right-3"}
-            style={{ backgroundColor: colour }}
-        >
-            {award}
-        </p>
-    )
+    if (award === "") {
+        return (<></>)
+    } else {
+        return (
+            <p
+                className={"font-bold text-white h-min px-3 py-1 rounded-full absolute bottom-3 right-3"}
+                style={{ backgroundColor: colour }}
+            >
+                {award}
+            </p>
+        )
+    }
 }
